46bdf7cee8f4d8478364da37b500605e159046de
Salela17 <marina.valiquette@gmail.com>
2017-07-04 19:41:39 -0400

    First commit

---
+++ frontend-nanodegree-mobile-portfolio-master/gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//Load plugins
+var gulp = require('gulp');
+//var rename = require("gulp-rename"); need to install
+var uglify = require('gulp-uglify');
+var minify = require('gulp-clean-css');
+
+gulp.task('min-css', function(){
+    return gulp.src('./css/*.css') // try css/*.css
+        //.pipe(concat('styles.css'))
+    //.pipe(rename({ suffix: '.min' })) // or .pipe.(rename(style.min.css)) if you have just one file
+        .pipe(minify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('./**/*.css',['min-css']);
+});
