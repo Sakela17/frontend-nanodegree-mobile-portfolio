2434073fa24d9578762886b80e3a0fc9838b5983
Salela17 <marina.valiquette@gmail.com>
2017-07-06 14:15:28 -0400

    Compressed images

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,26 @@ var gulp = require('gulp');
 //var rename = require("gulp-rename"); need to install
 var uglify = require('gulp-uglify');
 var minify = require('gulp-clean-css');
+//const imagemin = require('gulp-imagemin');
+const image = require('gulp-image');
+
+// gulp.task('min-img', () =>
+//     gulp.src('./img/*')
+//         .pipe(imagemin())
+//         .pipe(gulp.dest('dist/img'))
+// );
+
+// gulp.task('min-img', function() {
+//     return gulp.src('./img/*')
+//         .pipe(imagemin())
+//         .pipe(gulp.dest('dist/img'));
+// });
+
+gulp.task('min-img', function () {
+    gulp.src('./img/*')
+        .pipe(image())
+        .pipe(gulp.dest('dist/img'));
+});
 
 gulp.task('min-css', function(){
     return gulp.src('./css/*.css') // try css/*.css
